4eedd07e4ae2b4d8387b62ae8c5ba26cb1fc7962
Zunayed Hassan <zunayedhassan@appiomatic.com>
2024-05-10 17:17:45 +0600

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,13 @@
 // Feather Icon
 feather.replace();
+
+const sidebarMenuButton = document.querySelector("#edug-sidebar-menu-button");
+const sidebarMenuContents = document.querySelector("#edug-sidebar-menu-contents");
+
+sidebarMenuButton.addEventListener("click", event => {
+
+    let isCollapsed = sidebarMenuContents.getAttribute("data-isCollapsed") == "true";
+
+    sidebarMenuContents.setAttribute("data-isCollapsed", !isCollapsed);
+
+});
